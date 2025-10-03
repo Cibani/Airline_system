744cc440bd100d654bdc51ec2f209104f84ffc9c
Cibani <151187399+Cibani@users.noreply.github.com>
2025-10-03 22:53:59 +0530

    Delete frontend/script.js

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("songForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("In real app, this will send data to Java backend!");
-});
-
-window.onload = function() {
-    document.getElementById("songList").innerHTML = "<p>List of songs will appear here</p>";
-};
